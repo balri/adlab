13e4d6a5523c9065a52cefdfce37534c6c85abbe
Blair Venn <blair.venn@gmail.com>
2026-09-08 09:59:36 +1000

    Try to get refreshToken working from cookie

--- api/refresh.ts
+++ api/refresh.ts
@@ -8,7 +8,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 		return;
 	}
 
-	const currentRefreshToken = req.cookies.refreshToken;
+	const currentRefreshToken = req.cookies?.["__Secure-refreshToken"];
 
 	if (!currentRefreshToken) {
 		return res.status(401).json({ error: "No refresh token" });
